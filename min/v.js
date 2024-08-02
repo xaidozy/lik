811a8508f43cd38d8b655965046eb0e6cb3f2df7
xaidozy <xaidozy@proton.me>
2024-08-02 11:37:46 +0800

    '---'

---
+++ min/v.js
@@ -0,0 +1,22 @@
+$$ &&
+	$$('lik', {
+		xprefix: 'https://xaidozy.github.io/lik',
+		data: [
+			'Color_Code_F;フェルト;p',
+			'Clan3yang0yang0;Taylor Swift;Y;懒羊羊',
+			'Cping2fan2zhi1lu4;朴树;Y;平凡之路',
+			'Juice_World;;a',
+			'ONE_PUNCH_KO;;y',
+			'Climb_Ka;;a',
+			'A;;r',
+			'Others_i_World',
+			'The_Nights_Ever_Fading;DInxm Robinson;y',
+			'I_Walk_This_Earth_All_By_Myself;EKKSTACY;a',
+			'A4KEY;;a',
+			'Lava_Drinker;;W',
+			'Cxi3huan1he2ni3de1xia4tian1;;p;喜欢和你的夏天',
+			'Fatal_Love;Jori King;Y',
+			'POLICMR_LKW_SERGEAN;;p',
+			'Cxia4ge4lu4kou3zai4jian4ba1;;p;下个路口再见吧',
+		],
+	});
